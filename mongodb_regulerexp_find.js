167461d011800538a1a748f9ee68e50839b9ca97
selcukaltinay <selcukaltinay@outlook.com>
2022-03-13 17:23:44 +0300

    67th day completed

--- mongodb_regulerexp_find.js
+++ mongodb_regulerexp_find.js
@@ -4,8 +4,8 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  /*Return only the documents with the address "Park Lane 38":*/
-  var query = { address: "Park Lane 38" };
+  /*Return only the documents where the address starts with an "S":*/
+  var query = { address: /^S/ };
   dbo.collection("customers").find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
